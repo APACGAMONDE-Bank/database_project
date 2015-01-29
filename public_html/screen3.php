07597f04d8345ea7588215760634809271187571
Stefano Prezioso <stefprez@users.noreply.github.com>
2015-01-29 16:28:14 -0500

    Create screen3.php

---
+++ public_html/screen3.php
@@ -0,0 +1,62 @@
+
+<!-- Figure 3: Search Result Screen by Prithviraj Narahari, php coding: Alexander Martens -->
+<html>
+<head>
+	<title> Search Result - 3-B.com </title>
+	<script>
+	//redirect to reviews page
+	function review(isbn, title){
+		window.location.href="screen4.php?isbn="+ isbn + "&title=" + title;
+	}
+	//add to cart
+	function cart(isbn, searchfor, searchon, category){
+		window.location.href="screen3.php?cartisbn="+ isbn + "&searchfor=" + searchfor + "&searchon=" + searchon + "&category=" + category;
+	}
+	</script>
+</head>
+<body>
+	<table align="center" style="border:1px solid blue;">
+		<tr>
+			<td align="left">
+				
+					<h6> <fieldset>Your Shopping Cart has 0 items</fieldset> </h6>
+				
+			</td>
+			<td>
+				&nbsp
+			</td>
+			<td align="right">
+				<form action="shopping_cart.php" method="post">
+					<input type="submit" value="Manage Shopping Cart">
+				</form>
+			</td>
+		</tr>	
+		<tr>
+		<td style="width: 350px" colspan="3" align="center">
+			<div id="bookdetails" style="overflow:scroll;height:180px;width:400px;border:1px solid black;background-color:LightBlue">
+			<table>
+			<p>Sorry, no results found<br />Try a different search criteria</p>			</table>
+			</div>
+			
+			</td>
+		</tr>
+		<tr>
+			<td align= "center">
+				<form action="" method="get">
+					<input type="submit" value="Proceed To Checkout" id="checkout" name="checkout">
+				</form>
+			</td>
+			<td align="center">
+				<form action="screen2.php" method="post">
+					<input type="submit" value="New Search">
+				</form>
+			</td>
+			<td align="center">
+				<form action="screen1.php" method="post">
+					<input type="submit" name="exit" value="EXIT 3-B.com">
+				</form>
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
