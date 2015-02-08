1b1d29877159f7e9ded24f9906e8660d995a46f4
Cade Sperlich <csperlich@gmail.com>
2015-02-07 23:16:33 -0500

    add admin report page file and master css file

---
+++ public_html/report.php
@@ -0,0 +1,91 @@
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Reports</title>
+<link rel="stylesheet" type="text/css" href="master.css">
+</head>
+<body>
+	<h1>Administrator's Reports for 3-B</h1>
+	
+	<form id="cancel" action="welcome.php" method="post">
+		<input type="submit" id="exit" name="exit" value="EXIT 3-B.com">
+	</form>
+
+	<h2>Registerd Customers</h2>
+	<table>
+		<tr>
+			<th>
+				Total # of Customers
+			</th>
+		</tr>
+		<tr>
+			<td>
+				Placeholder: No Customers
+			</td>
+		</tr>
+	</table>
+
+	<h2>Inventory by Genre</h2>
+	<table>
+		<tr>
+			<th>
+				Genre
+			</th>
+			<th>
+				# of Books
+			</th>
+		</tr>
+		<tr>
+			<td>
+				Placeholder: No Genres
+			</td>
+			<td>
+				Placeholder: No Books
+			</td>
+		</tr>
+	</table>
+
+	<h2>Monthly Sales for Placeholder: year</h2>
+	<table>
+		<tr>
+			<th>
+				Month
+			</th>
+			<th>
+				Sales
+			</th>
+		</tr>
+		<tr>
+			<td>
+				Placeholder: December
+			</td>
+			<td>
+				Placeholder: No Sales
+			</td>
+		</tr>
+	</table>
+
+	<h2>Book Reviews</h2>
+	<table>
+		<tr>
+			<th>
+				Book
+			</th>
+			<th>
+				# of Reviews
+			</th>
+		</tr>
+		<tr>
+			<td>
+				Placeholder: Book Title
+			</td>
+			<td>
+				Placeholder: No Reviews
+			</td>
+		</tr>
+	</table>
+
+</body>
+
+</html>
