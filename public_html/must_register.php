0e062d35dbb604e223cbe1c029caf0f645b3697d
Cade Sperlich <csperlich@gmail.com>
2015-02-08 00:21:32 -0500

    add must_register.php warning page for when user exits registration before making a purchase

---
+++ public_html/must_register.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>You must register before purchasing</title>
+</head>
+<body>
+	<h2>In order to proceed with the payment, you need to register first.</h2>
+	<form action="search.php"> 
+		<input type="submit" value="OK">
+	</form>
+</body>
+</html>
