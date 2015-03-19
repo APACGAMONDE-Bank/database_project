01ed6c8511cbf7d6b3a9294fbae6d8a6085ddbaf
Stefano Prezioso <stefprez@gmail.com>
2015-03-19 16:46:00 -0400

    Initial commit.

---
+++ DML_Statements.sql
@@ -0,0 +1,7 @@
+INSERT INTO customer 
+		(username, pin, first_name, last_name)
+	VALUES 
+		('jsmith', 0000, 'John', 'Smith'),
+		('sdouglas', 0000, 'Sue', 'Douglas'),
+		('mjackson', 0000, 'Mark', 'Jackson');
+
