71ebf27b2064ea81ff9248e2490dec621a99baf1
Stefano Prezioso <stefprez@gmail.com>
2015-03-19 16:14:47 -0400

    Added `NOT NULL` restrictions.

--- DDL_Statements.sql
+++ DDL_Statements.sql
@@ -8,14 +8,14 @@ SET foreign_key_checks=0;
 
 CREATE TABLE customer
 	(
-		username VARCHAR(20), 
-		pin INT, 
+		username VARCHAR(20) NOT NULL, 
+		pin INT NOT NULL, 
 		card_type VARCHAR(15), 
 		card_number INT, 
 		card_exp_date DATE, 
-		first_name VARCHAR(20), 
+		first_name VARCHAR(20) NOT NULL, 
 		middle_name VARCHAR(20), 
-		last_name VARCHAR(20), 
+		last_name VARCHAR(20) NOT NULL, 
 		street VARCHAR(20), 
 		city VARCHAR(20), 
 		state CHAR(2), 
@@ -25,8 +25,8 @@ CREATE TABLE customer
 
 CREATE TABLE book
 	(
-		isbn INT,
-		title VARCHAR(50),
+		isbn INT NOT NULL,
+		title VARCHAR(50) NOT NULL,
 		price DECIMAL(6,2),
 		category VARCHAR(20),
 		pub_date DATE,
@@ -38,34 +38,34 @@ CREATE TABLE book
 CREATE TABLE publisher
 	(
 		pub_id INT NOT NULL AUTO_INCREMENT,
-		name VARCHAR(30),
+		name VARCHAR(30) NOT NULL,
 		PRIMARY KEY (pub_id)
 	);
 
 CREATE TABLE author
 	(
 		author_id INT NOT NULL AUTO_INCREMENT,
-		first_name VARCHAR(20),
+		first_name VARCHAR(20) NOT NULL,
 		middle_name VARCHAR(20),
-		last_name VARCHAR(20),
+		last_name VARCHAR(20) NOT NULL,
 		PRIMARY KEY (author_id)
 	);
 
 CREATE TABLE invoice
 	(
 		invoice_id INT NOT NULL AUTO_INCREMENT,
-		sale_datetime DATETIME,
-		shipping_cost DECIMAL(6,2),
-		grand_total DECIMAL(6,2),
-		username VARCHAR(20),
+		sale_datetime DATETIME NOT NULL,
+		shipping_cost DECIMAL(6,2) NOT NULL,
+		grand_total DECIMAL(6,2) NOT NULL,
+		username VARCHAR(20) NOT NULL,
 		FOREIGN KEY (username) REFERENCES customer(username),
 		PRIMARY KEY (invoice_id)
 	);
 
 CREATE TABLE written_by
 	(
-		isbn INT,
-		author_id INT,
+		isbn INT NOT NULL,
+		author_id INT NOT NULL,
 		FOREIGN KEY (isbn) REFERENCES book(isbn),
 		FOREIGN KEY (author_id) REFERENCES author(author_id),
 		PRIMARY KEY (isbn, author_id)
@@ -73,9 +73,9 @@ CREATE TABLE written_by
 
 CREATE TABLE cart_items
 	(
-		username VARCHAR(20),
-		isbn INT,
-		quantity INT,
+		username VARCHAR(20) NOT NULL,
+		isbn INT NOT NULL,
+		quantity INT NOT NULL,
 		FOREIGN KEY (username) REFERENCES customer(username),
 		FOREIGN KEY (isbn) REFERENCES book(isbn),
 		PRIMARY KEY (username, isbn)
@@ -83,10 +83,10 @@ CREATE TABLE cart_items
 
 CREATE TABLE reviews
 	(
-		username VARCHAR(20),
-		isbn INT,
+		username VARCHAR(20) NOT NULL,
+		isbn INT NOT NULL,
 		comment TEXT, 
-		rating INT,
+		rating INT NOT NULL,
 		FOREIGN KEY (username) REFERENCES customer(username),
 		FOREIGN KEY (isbn) REFERENCES book(isbn),
 		PRIMARY KEY (username, isbn)
@@ -94,10 +94,10 @@ CREATE TABLE reviews
 
 CREATE TABLE invoice_items
 	(
-		invoice_id INT,
-		isbn INT,
-		quantity INT,
-		price_at_purchase DECIMAL(6,2),
+		invoice_id INT NOT NULL,
+		isbn INT NOT NULL,
+		quantity INT NOT NULL,
+		price_at_purchase DECIMAL(6,2) NOT NULL,
 		FOREIGN KEY (invoice_id) REFERENCES invoice(invoice_id),
 		FOREIGN KEY (isbn) REFERENCES book(isbn),
 		PRIMARY KEY (invoice_id, isbn)
@@ -105,7 +105,7 @@ CREATE TABLE invoice_items
 
 CREATE TABLE web_admin
 	(
-		username VARCHAR(20),
-		password VARCHAR(32),
+		username VARCHAR(20) NOT NULL,
+		password VARCHAR(32) NOT NULL,
 		PRIMARY KEY (username)
 	);
