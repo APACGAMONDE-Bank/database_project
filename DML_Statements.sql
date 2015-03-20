9aa65e3bb25d4fb0eeac4b87eba96d264c4f8008
Stefano Prezioso <stefprez@gmail.com>
2015-03-19 20:22:42 -0400

    Finished DML, added missing auto-increment to invoice_id.

--- DML_Statements.sql
+++ DML_Statements.sql
@@ -1,9 +1,12 @@
 TRUNCATE customer;
 TRUNCATE book;
-TRUNCATE author;
 TRUNCATE publisher;
+TRUNCATE author;
+TRUNCATE invoice;
 TRUNCATE written_by;
+TRUNCATE cart_items;
 TRUNCATE reviews;
+TRUNCATE invoice_items;
 TRUNCATE web_admin;
 
 INSERT INTO customer 
@@ -34,6 +37,13 @@ INSERT INTO author
 		('Dan', "Brown"),
 		('Lois', 'Lowry');
 
+INSERT INTO invoice
+		(sale_datetime, shipping_cost, grand_total, username)
+	VALUES
+		('2015-03-14 09:26:53', 3.99, 18.98, 'jsmith'),
+		('2014-12-20 19:46:51', 3.99, 23.94, 'sdouglas'),
+		('2013-05-16 22:00:01', 2.99, 12.98, 'mjackson');
+
 INSERT INTO written_by
 		(isbn, author_id)
 	VALUES
@@ -41,12 +51,26 @@ INSERT INTO written_by
 		('9780307474278', 2),
 		('9780544336261', 3);
 
+INSERT INTO cart_items
+		(username, isbn, quantity)
+	VALUES
+		('jsmith', '9780590353427', 1),
+		('sdouglas', '9780307474278', 2),
+		('mjackson', '9780544336261', 3);
+
 INSERT INTO reviews
 		(username, isbn, comment, rating)
 	VALUES
 		('jsmith', '9780590353427', 'Magical!', 5),
 		('sdouglas', '9780307474278', 'Action packed.', 4),
 		('mjackson', '9780544336261', 'Insightful.', 3);
+
+INSERT INTO invoice_items
+		(isbn, quantity, price_at_purchase)
+	VALUES
+		('9780590353427', 1, 14.99),
+		('9780307474278', 1, 19.95),
+		('9780544336261', 1, 19.99);
 
 INSERT INTO web_admin
 		(username, password)
