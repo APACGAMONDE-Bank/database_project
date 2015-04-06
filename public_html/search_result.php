d214c0cc82dd1c701000aad1e9e49670bb8af39b
Stefano Prezioso <stefprez@gmail.com>
2015-04-06 01:24:01 -0400

    Added search functionality and add to cart function.

--- public_html/search_result.php
+++ public_html/search_result.php
@@ -1,53 +1,95 @@
-<!-- Figure 3: Search Result Screen by Prithviraj Narahari, php coding: Alexander Martens -->
+<?php
+	require_once('php_tools.php');
+	require_once('header.php');
+
+	function addToCart($isbn)
+	{
+		if (isset($_SESSION['username']))
+		{
+			// Database Connection
+			$servername = "localhost";
+			$db_username = "201501_471_02";
+			$password = "cade&stefano";
+			$database = "db201501_471_g02";
+
+			try {
+				$databaseConnection = new PDO("mysql:host=$servername;dbname=$database", $db_username, $password);
+				
+				// set the PDO error mode to exception
+				$databaseConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			}
+			catch(PDOException $e) {
+				echo "Connection failed: " . $e->getMessage();
+			}
+
+			$username = $_SESSION['username'];
+			$addToCartStatement = $databaseConnection->prepare("INSERT INTO cart_items (username, isbn, quantity) VALUES ('$username', '$isbn', 1)");
+			$addToCartStatement->execute();
+		}
+		else {
+			if (! isset($_SESSION['cart_items'])) {
+				echo "making new session cart";
+				$_SESSION['cart_items'] = new SessionCartItems();
+			}
+			
+			$sessionCart = $_SESSION['cart_items'];
+			$sessionCart->addToCart($isbn);
+		}
+		echo "<script> alert('ISBN: $isbn added to your cart.'); </script>";
+	}
+
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		addToCart($_POST['isbn']);
+	}
+
+	// Database Connection
+	$servername = "localhost";
+	$username = "201501_471_02";
+	$password = "cade&stefano";
+	$database = "db201501_471_g02";
+
+	try {
+		$databaseConnection = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+		
+		// set the PDO error mode to exception
+		$databaseConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+	catch(PDOException $e) {
+		echo "Connection failed: " . $e->getMessage();
+	}
+
+	// Search term setup
+	$searchTerm = $_GET['searchfor'];
+	$searchAttribute = $_GET['searchon'];
+	$searchCategory = $_GET['category'];
+	
+	if ($searchAttribute === 'anywhere'){ //search term can match publisher, author, isbn, or title
+		if($searchCategory === 'all'){ //category is unspecified
+			$searchStatement = $databaseConnection->prepare("SELECT * FROM (book NATURAL JOIN author NATURAL JOIN written_by NATURAL JOIN publisher) WHERE isbn LIKE '%$searchTerm%' OR title LIKE '%$searchTerm%' OR first_name LIKE '%$searchTerm%' OR middle_name LIKE '%$searchTerm%' OR last_name LIKE '%$searchTerm%' OR name LIKE '%$searchTerm%'");
+		} 
+		else { //category is specified
+			$searchStatement = $databaseConnection->prepare("SELECT * FROM (book NATURAL JOIN author NATURAL JOIN written_by NATURAL JOIN publisher) WHERE category = '$searchCategory' AND (isbn LIKE '%$searchTerm%' OR title LIKE '%$searchTerm%' OR first_name LIKE '%$searchTerm%' OR middle_name LIKE '%$searchTerm%' OR last_name LIKE '%$searchTerm%' OR name LIKE '%$searchTerm%')");
+		}
+	} 
+	else { //seach attribute is specified
+		if($searchCategory === 'all'){ //category is unspecified
+			$searchStatement = $databaseConnection->prepare("SELECT * FROM (book NATURAL JOIN author NATURAL JOIN written_by NATURAL JOIN publisher) WHERE $searchAttribute LIKE '%$searchTerm%'");
+		} 
+		else { //category is specified
+			$searchStatement = $databaseConnection->prepare("SELECT * FROM (book NATURAL JOIN author NATURAL JOIN written_by NATURAL JOIN publisher) WHERE category = '$searchCategory' AND $searchAttribute LIKE '%$searchTerm%'");
+		}
+	}
+
+?>
 <html>
 <head>
 	<title> Search Result - 3-B.com </title>
-	<script>
-	//redirect to reviews page
-	function review(isbn, title){
-		window.location.href="review.php?isbn="+ isbn + "&title=" + title;
-	}
-	//add to cart
-	function cart(isbn, searchfor, searchon, category){
-		window.location.href="search_result.php?cartisbn="+ isbn + "&searchfor=" + searchfor + "&searchon=" + searchon + "&category=" + category;
-	}
-	</script>
 </head>
 <body>
-
-
-	<?php
-		/*
-		foreach ( $_GET as $key => $value ) 
-		{
-
-    		echo 'Key : ' . $key . ', Value : ' . $value;
-    		echo '<br/>';
-		}
-		*/
-
-		$servername = "localhost";
-		$username = "201501_471_02";
-		$password = "cade&stefano";
-		$database = "db201501_471_g02";
-
-		try {
-    		$conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-    		
-    		// set the PDO error mode to exception
-    		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    	}
-		catch(PDOException $e) {
-    		echo "Connection failed: " . $e->getMessage();
-    	}
-	?>
-
 	<table align="center" style="border:1px solid blue;">
 		<tr>
 			<td align="left">
-				
 					<h6> <fieldset>Your Shopping Cart has 0 items</fieldset> </h6>
-				
 			</td>
 			<td>
 				&nbsp
@@ -62,41 +104,25 @@
 		<td style="width: 350px" colspan="3" align="center">
 			<div id="bookdetails" style="overflow:scroll;height:180px;width:400px;border:1px solid black;background-color:LightBlue">
 			<table style="font-size:x-small;">
+			<form action="" method="POST">
 			<?php
-
-				$searchTerm = $_GET['searchfor'];
-				$searchAttribute = $_GET['searchon'];
-				$searchCategory = $_GET['category'];
-				
-				if ($searchAttribute === 'anywhere'){ //search term can match publisher, author, isbn, or title
-					if($searchCategory === 'all'){ //category is unspecified
-						$stmt = $conn->prepare("SELECT * FROM book WHERE isbn LIKE '%$searchTerm%' OR title LIKE '%$searchTerm%' OR author_name LIKE '%$searchTerm%' OR publisher_name LIKE '%$searchTerm%'");
-					} else { //category is specified
-						$stmt = $conn->prepare("SELECT * FROM book WHERE category = '$searchCategory' AND (isbn LIKE '%$searchTerm%' OR title LIKE '%$searchTerm%' OR author_name LIKE '%$searchTerm%' OR publisher_name LIKE '%$searchTerm%')");
-					}
-				} else { //seach attribute is specified
-					if($searchCategory === 'all'){ //category is unspecified
-						$stmt = $conn->prepare("SELECT * FROM book WHERE $searchAttribute LIKE '%$searchTerm%'");
-					} else { //category is specified
-						$stmt = $conn->prepare("SELECT * FROM book WHERE category = '$searchCategory' AND $searchAttribute LIKE '%$searchTerm%'");
-					}
-				}
-
 				$resultCount = 0;
-    			if($stmt->execute()) {
-    				while ($row = $stmt->fetch()) {
+    			if($searchStatement->execute()) {
+    				while ($row = $searchStatement->fetch()) {
     					$resultCount++;
     					print "<tr>";
     						print '<td>';
     							print '<table>';
     								print "<tr>";
     									print "<td>";
-    										print '<button type="button" >Add to Cart</button>';
+    											print '<input type="submit" class="button" value="Add To Cart"/>';
+    											print '<input type="hidden" name="isbn" id="isbn" value="' . $row['isbn'] . '"/>';
+    										print '</form>';
     									print "</td>";
     								print "</tr>";
     								print "<tr>";
     									print "<td>";
-    										print '<form action="review.php" method="GET">';
+    										print '<form action="review.php" method="POST">';
     											print '<input type="hidden" value="' . $row['isbn'] . '">';
     											print '<input type="submit" value="Reviews">';
     										print '</form>';
